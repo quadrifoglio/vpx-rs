25d36d486e64003e58f718ccb2c5a3b30201f5fd
quadrifoglio <quadrifoglio.clement@protonmail.com>
2017-12-17 14:21:13 +0100

    started retreiving decoded frames

--- vpx/src/decoder.rs
+++ vpx/src/decoder.rs
@@ -4,6 +4,7 @@ use std::ptr;
 use std::mem;
 
 use lib;
+use super::Image;
 use error::{self, Result};
 
 /// Decoder object (context).
@@ -32,6 +33,50 @@ impl Decoder {
             Ok(Decoder {
                 ctx: ctx,
             })
+        }
+    }
+
+    /// Decode the specified VPX data. Returns an iterator over the available frames that have been
+    /// decoded.
+    pub fn decode(&mut self, data: Vec<u8>) -> Result<Frames> {
+        unsafe {
+            let ptr = data.as_ptr();
+            let size = data.len() as u32;
+            let err = lib::vpx_codec_decode(&mut self.ctx, ptr, size, ptr::null_mut(), 0);
+
+            if !error::ok(err) {
+                bail!(error::libvpx(err));
+            }
+
+            let frames = Frames {
+                dec: self,
+                iter: ptr::null_mut(),
+            };
+
+            Ok(frames)
+        }
+    }
+}
+
+pub struct Frames<'a> {
+    dec: &'a mut Decoder,
+    iter: *mut lib::vpx_codec_iter_t,
+}
+
+impl<'a> ::std::iter::Iterator for Frames<'a> {
+    type Item = Image;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            let img = lib::vpx_codec_get_frame(&mut self.dec.ctx, self.iter);
+
+            if img == ptr::null_mut() {
+                None
+            } else {
+                Some(Image {
+                    img: img,
+                })
+            }
         }
     }
 }
--- vpx/src/lib.rs
+++ vpx/src/lib.rs
@@ -10,3 +10,24 @@ pub mod error;
 pub mod decoder;
 
 pub use decoder::Decoder;
+
+/// Represents a clear video image.
+pub struct Image {
+    img: *mut lib::vpx_image_t,
+}
+
+impl Image {
+    /// Return the width of the image in pixels.
+    pub fn width(&self) -> u32 {
+        unsafe {
+            (*self.img).w as u32
+        }
+    }
+
+    /// Return the height of the image in pixels.
+    pub fn height(&self) -> u32 {
+        unsafe {
+            (*self.img).h as u32
+        }
+    }
+}
